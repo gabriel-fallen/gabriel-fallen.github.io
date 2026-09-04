547877ffbea19e029193665be084814080872e42
Alex Chichigin <sad.ronin@gmail.com>
2026-09-04 19:19:40 +0400

    feat: threads description

--- src/lib/components/threads.svelte
+++ src/lib/components/threads.svelte
@@ -10,9 +10,17 @@
 <section class="section">
   <h2 class="title">Threads</h2>
 
+  <p class="pb-4">
+    My career looks chaotic: many programming languages and tech stacks, from embedded to full-stack Web development
+    with machine learning mixed in, from industry to academia, then back to industry — twice. And yet, there's
+    a handful of threads holding this tapestry together. To make them visible, I turned them into filters or lenses
+    through which you can view my work experience and projects. Click the tags below and see which jobs and projects
+    share the same threads.
+  </p>
+
   <div class="tags">
     {#each threads as t}
-      <button class={($active.includes(t)) ? "tag is-hoverable is-dark" : "tag is-is-hoverable"} onclick={() => toggle(t)}>{t}</button>
+      <button class={($active.includes(t)) ? "tag is-medium is-hoverable is-dark" : "tag is-medium is-is-hoverable"} onclick={() => toggle(t)}>{t}</button>
     {/each}
   </div>
 </section>
